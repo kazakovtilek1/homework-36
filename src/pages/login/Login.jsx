1c09c03762f42c540b27dfeef3333d491750f26a
Tilek <tilek.19990223@gmail.com>
2024-05-20 23:55:22 +0600

    Router-dom-hooks

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useNavigate} from "react-router-dom";
 
 function Login() {
 
     const navigate = useNavigate();
 
-    function login (e) {
-        e.preventDefault();
+    function login () {
         setTimeout(() => {
             navigate("/")
         }, 3000)
     }
 
+    useEffect(() => {
+        login()
+    }, []);
 
     return (
         <div>
             <h2>Login</h2>
-            <button onClick={login}>Main</button>
         </div>
     );
 }
